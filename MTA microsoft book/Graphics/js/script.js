3f5a232b01fa926aad76509ac209ed7d9ab874ec
YordanPetkov <49204099+YordanPetkov@users.noreply.github.com>
2019-04-09 18:40:58 +0300

    Add files via upload

---
+++ MTA microsoft book/Graphics/js/script.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+
+function FillRectFunc() {
+    var canvas = document.getElementById("SmlRect1");
+    ctx = canvas.getContext("2d");
+    ctx.
+    ctx.fillStyle = "rgb(0,0,255)";
+    
+    ctx.fillRect(0, 0, 500, 400);
+}
